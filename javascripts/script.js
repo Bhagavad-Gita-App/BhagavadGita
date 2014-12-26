c4724ada59e9fc50459a826916e496db9a5b353c
Floyd Pink <imbleedingme@googlemail.com>
2014-12-26 13:41:32 -0500

    static site ready (?)

--- javascripts/script.js
+++ javascripts/script.js
@@ -16,59 +16,15 @@ var getQ           = function (name) {
       }
       return ((slokaNumber.toString().indexOf(',') > -1) ? 'ശ്ലോകങ്ങൾ ' : 'ശ്ലോകം ') + slokaNumber;
     },
-    shareReady     = function () { // jshint ignore:line
-      $.getJSON('../javascripts/gita.json', function (gita) {
-        var c = getQ('c'), s = getQ('s');
-        var chapter = gita.Chapters[c];
-        var title = '';
-        var description = '';
-        if (c && s && chapter && gita.Chapters[c].Sections[s]) {
-          var sloka = chapter.Sections[s];
-          $('.speaker').html(sloka.Speaker || '');
-          $('.sloka').html(sloka.Content);
-          $('.meaning').html(sloka.Meaning);
-          $('#subtitle').html(chapter.Subtitle ? chapter.Subtitle + ' - ' : '');
-          $('#title').html(chapter.Title);
-          var slokaNumber = getSlokaNumber(s, sloka);
-          $('#slokaNumber').html(' - ' + slokaNumber);
-          title = 'ശ്രീമദ് ഭഗവദ്ഗീത - ' + $('#title').html() + $('#slokaNumber').html();
-          description = sloka.Content;
-          $('#share').fadeIn();
-        } else if (c && chapter) {
-          var sectionClone = $('#section').detach();
-          $.each(chapter.Sections, function (sectionIndex, sloka) {
-            var slokaCopy = sectionClone.clone().attr('id', 'section' + sectionIndex).appendTo('#chapter');
-            slokaCopy.find('.speaker').html(sloka.Speaker || '');
-            slokaCopy.find('.sloka').html(sloka.Content);
-            slokaCopy.find('.meaning').html(sloka.Meaning);
-          });
-          $('#title').html(chapter.Title);
-          title = 'ശ്രീമദ് ഭഗവദ്ഗീത - ' + $('#title').html();
-          description = $('#section0').find('.sloka').html();
-          $('#share').fadeIn();
-        }
-        document.title = title;
-        description = description.length > 117 ? description.substring(0, 117) + '...' : description;
-        $('#fbsharebutton').attr('data-href', location.href);
-        // facebook open graph
-        $('head').append('<meta property="og:title" content="' + title + '" />');
-        $('head').append('<meta property="og:url" content="' + location.href + '" />');
-        $('head').append('<meta property="og:description" content="' + description + '"  />');
-        // twitter cards
-        $('meta[name="twitter:url"]').attr('content', location.href);
-        $('meta[name="twitter:title"]').attr('content', title);
-        $('meta[name="twitter:description"]').attr('content', description);
-      });
-    },
     allReady       = function () { // jshint ignore:line
       $(function () {
         $.getJSON('./javascripts/gita.json', function (gita) {
           var links = [];
           $.each(gita.Chapters, function (chapterIndex, chapter) {
-            links.push('<li class="title"><a href="./share/?c=' + chapterIndex + '">' + chapter.Title + '</a></li>');
+            links.push('<li class="title"><a href="./share/' + chapterIndex + '/">' + chapter.Title + '</a></li>');
             $.each(chapter.Sections, function (sectionIndex, section) {
               var linkText = getSlokaNumber(sectionIndex, section);
-              links.push('<li><a href="./share/?c=' + chapterIndex + '&s=' + sectionIndex + '">' + linkText + '</a></li>');
+              links.push('<li><a href="./share/' + chapterIndex + '/' + sectionIndex + '/">' + linkText + '</a></li>');
             });
           });
           $('#links').append(links.join(''));
